8cf9b02d654f436c06878bb88507784eaa62853d
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-02-06 11:36:44 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+let originalImg = "/";
+let itemList = "";
 
-console.log(galleryItems);
+
+galleryItems.forEach(({ preview, original, description }) => {
+  itemList += `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+});
+document.querySelector(".gallery").innerHTML = itemList;
+
+document.querySelector(".gallery").addEventListener("click", (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault()
+//   originalImg = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+  instance.show()
+  console.log(event.target.dataset.source);
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+let itemList = "";
+
+galleryItems.forEach(({ preview, original, description }) => {
+  itemList += `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </li>`;
+});
+document.querySelector(".gallery").innerHTML = itemList;
+function elemnetForCaptions() {
+    return document.querySelectorAll('a.gallery_item')
+}
+document.querySelector(".gallery").addEventListener("click", (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+
+  let lightbox = new SimpleLightbox('.gallery a', {
+                                                    captionsData: 'alt', 
+                                                    captionDelay: 250
+                                                })
+  console.log(lightbox);
+});
